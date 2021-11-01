90a0125aa7a035b1044fccc648463425b76ff8d3
Tien Doan Manh <dmtien@cmc.com.vn>
2021-11-01 16:44:58 +0700

    add unit test delete redundant solutions

---
+++ BookStore/BookStore.Test/CategoryServiceTests.cs
@@ -0,0 +1,68 @@
+using AutoMapper;
+using BookStore.Contract.ResponseModels;
+using BookStore.Domain.Models;
+using BookStore.Repository.Interfaces;
+using BookStore.Service.Category;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore.Test
+{
+    [TestClass]
+    public class CategoryServiceTests
+    {
+        [TestMethod]
+        public void TestGetAllCategories()
+        {
+            // Arrange
+            var allCategories = new List<Category>
+            {
+                new Category
+                {
+                    CategoryId = Guid.NewGuid(),
+                    CategoryName = "CategoryName",
+                    Description = "Description",
+                    Books = new List<Book>()
+                }
+            };
+
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            mockCategoryRepository.Setup(repository => repository.GetAllCategories(It.IsAny<string>()))
+                .Returns(allCategories);
+
+            var mockMapper = new Mock<IMapper>();
+            mockMapper.Setup(mapper => mapper.Map<List<Category>, List<CategoryResponseModel>>(allCategories))
+                .Returns(new List<CategoryResponseModel>
+                {
+                    new CategoryResponseModel
+                    {
+                        CategoryId = Guid.NewGuid(),
+                        CategoryName = "CategoryName",
+                        Description = "Description",
+                        Books = new List<BookResponseModel>()
+                    },
+                    new CategoryResponseModel
+                    {
+                        CategoryId = Guid.NewGuid(),
+                        CategoryName = "CategoryName",
+                        Description = "Description",
+                        Books = new List<BookResponseModel>()
+                    }
+                });
+
+            var categoryService = new CategoryService(mockCategoryRepository.Object, mockMapper.Object);
+
+            // Act
+            var results = categoryService.GetAllCategories("x");
+
+            // Assert
+            Assert.AreEqual(2, results.Count);
+
+            mockCategoryRepository.Verify(repository => repository.GetAllCategories("x"), Times.Once);
+
+            mockMapper.Verify(repository => repository.Map<List<Category>, List<CategoryResponseModel>>(allCategories), Times.Once);
+        }
+    }
+}
