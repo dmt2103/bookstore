dd35b4bfa7d6e23ca956e8c16c9f19e05aa939d3
Tien Doan Manh <dmtien@cmc.com.vn>
2021-11-02 17:10:24 +0700

    update

--- BookStore/BookStore.Test/ServiceTests/CategoryServiceTests.cs
+++ BookStore/BookStore.Test/ServiceTests/CategoryServiceTests.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using BookStore.Contract.RequestModels;
 using BookStore.Contract.ResponseModels;
 using BookStore.Domain.Models;
 using BookStore.Repository.Interfaces;
@@ -8,7 +9,7 @@ using Moq;
 using System;
 using System.Collections.Generic;
 
-namespace BookStore.Test
+namespace BookStore.Test.ServiceTests
 {
     [TestClass]
     public class CategoryServiceTests
@@ -63,6 +64,44 @@ namespace BookStore.Test
             mockCategoryRepository.Verify(repository => repository.GetAllCategories("x"), Times.Once);
 
             mockMapper.Verify(repository => repository.Map<List<Category>, List<CategoryResponseModel>>(allCategories), Times.Once);
+        }
+
+        [TestMethod]
+        public void TestCreateCategory()
+        {
+            // Arrange
+            var categoryRequestModel = new CategoryRequestModel();
+            var category = new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                CategoryName = "CategoryName",
+                Description = "Description",
+                Books = new List<Book>()
+            };
+
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            mockCategoryRepository.Setup(repository => repository.CreateCategory(categoryRequestModel))
+                .Returns(category);
+
+            var mockMapper = new Mock<IMapper>();
+            mockMapper.Setup(mapper => mapper.Map<Category, CategoryResponseModel>(category))
+                .Returns(new CategoryResponseModel
+                {
+                    CategoryId = Guid.NewGuid(),
+                    CategoryName = "CategoryName",
+                    Description = "Description",
+                    Books = new List<BookResponseModel>()
+                });
+
+            var categoryService = new CategoryService(mockCategoryRepository.Object, mockMapper.Object);
+
+            // Act
+            var results = categoryService.CreateCategory(categoryRequestModel);
+
+            // Assert
+            mockCategoryRepository.Verify(repository => repository.CreateCategory(categoryRequestModel), Times.Once);
+
+            mockMapper.Verify(repository => repository.Map<Category, CategoryResponseModel>(category), Times.Once);
         }
     }
 }
